e4ddbf154de1e3fd98a52b0a090ccd37f44c1c1a
Elnur Urazymbetov <311293e@gmail.com>
2022-06-15 20:29:00 +0600

    bundle file added

--- src/UelnurSymfonyViewControllerBundle.php
+++ src/UelnurSymfonyViewControllerBundle.php
@@ -2,6 +2,7 @@
 
 namespace Uelnur\SymfonyViewController;
 
+use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -10,17 +11,16 @@ class UelnurSymfonyViewControllerBundle extends Bundle {
     public function build(ContainerBuilder $container) {
         parent::build($container);
 
-        $definitions[] = (new Definition(ViewRouteManager::class))
+        $definitions[ViewRouteManager::class] = (new Definition(ViewRouteManager::class))
             ->setAutoconfigured(true)
             ->setAutowired(true);
 
-        $definitions[] = (new Definition(ViewControllerSubscriber::class))
+        $definitions[ViewControllerSubscriber::class] = (new Definition(ViewControllerSubscriber::class))
             ->setAutoconfigured(true)
-            ->setAutowired(
-            true);
+            ->setAutowired(true);
 
         $container->addDefinitions($definitions);
-        $container->registerForAutoconfiguration(ViewMiddlewareInterface::class)->addTag('uelnur_view_controller.middleware');
-        $container->registerForAutoconfiguration(ViewInterface::class)->addTag('uelnur_view_controller.view');
+        $container->registerForAutoconfiguration(ViewMiddlewareInterface::class)->addTag('uelnur.view.middleware');
+        $container->registerForAutoconfiguration(ViewInterface::class)->addTag('uelnur.view');
     }
 }
