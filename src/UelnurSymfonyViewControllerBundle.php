c7a339ee8d1810ef2a568d5908cc47e180625956
Elnur Urazymbetov <311293e@gmail.com>
2022-06-15 20:20:11 +0600

    bundle file added

--- src/UelnurSymfonyViewControllerBundle.php
+++ src/UelnurSymfonyViewControllerBundle.php
@@ -8,8 +8,16 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UelnurSymfonyViewControllerBundle extends Bundle {
     public function build(ContainerBuilder $container) {
-        $definitions[] = new Definition(ViewControllerSubscriber::class);
-        $definitions[] = new Definition(ViewRouteManager::class);
+        parent::build($container);
+
+        $definitions[] = (new Definition(ViewControllerSubscriber::class))
+            ->setAutoconfigured(true)
+            ->setAutowired(
+            true);
+
+        $definitions[] = (new Definition(ViewRouteManager::class))
+            ->setAutoconfigured(true)
+            ->setAutowired(true);
 
         $container->addDefinitions($definitions);
         $container->registerForAutoconfiguration(ViewMiddlewareInterface::class)->addTag('uelnur_view_controller.middleware');
